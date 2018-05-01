28ff2233bddd7e867014fc35e0e07d9f46884df9
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 19:57:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.split.count > 0
+      self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+    else
+      0
+    end
   end
 end
